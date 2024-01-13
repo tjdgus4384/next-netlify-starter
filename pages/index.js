3318c3e834602c9f339907060718c32f802451de
tjdgus4384 <42888814+tjdgus4384@users.noreply.github.com>
2024-01-13 11:44:38 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
-
 export default function Home() {
   return (
     <div className="container">
